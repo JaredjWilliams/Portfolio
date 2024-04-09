af967467f8c511f6a38d5632aebbd760cfe72432
Jared Williams <jared.williams101697@gmail.com>
2024-04-09 12:25:51 -0400

    Added contact me page with server setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import {AboutComponent} from "./views/about/about.component";
 import {ProjectsComponent} from "./views/projects/projects.component";
 import {ResumeComponent} from "./views/resume/resume.component";
 import {ErrorComponent} from "./views/error/error.component";
+import {ContactMeComponent} from "./views/contact_me/contact-me.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "about", component: AboutComponent },
   { path: "resume", component: ResumeComponent },
   { path: "projects", component: ProjectsComponent },
+  { path: "contact", component: ContactMeComponent },
 
   {path: "**", component: ErrorComponent}
 
